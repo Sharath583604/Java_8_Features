15308e5202848a9446d24688637295d088c6893b
Sharath583604 <sharathjogi04@gmail.com>
2025-08-06 18:30:28 +0530

    map

--- src/com/streamApi/StreamApi.java
+++ src/com/streamApi/StreamApi.java
@@ -6,10 +6,14 @@ import java.util.stream.Stream;
 
 public class StreamApi {
 	public static void main(String[] args) {
+		
+		//STREAM 
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		Stream<Integer> stream = list.stream();
 		stream.filter(i -> (i % 2 == 0)).forEach(System.out::println);
-
+		
+		
+		//FILTER
 		List<Integer> oddnumbers = list.stream().filter(i -> (i % 2 != 0)).toList();
 		oddnumbers.forEach(System.out::println);
 
@@ -24,6 +28,7 @@ public class StreamApi {
 		List<Employee> fist = emp.stream().filter(i -> i.ename.startsWith("s")).toList();
 		fist.forEach(n -> System.out.println(n.ename));
 		
+		//MAP
 		emp.stream().filter(i -> i.ename.startsWith("s")).map(n->n.ename.toUpperCase()).forEach(System.out::println);;
 		
 		list.stream().map(n->n*n).forEach(System.out::println);
