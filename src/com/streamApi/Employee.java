cd0738bca49a2c975af3a28938accdfff28180a6
Sharath583604 <sharathjogi04@gmail.com>
2025-08-06 18:17:18 +0530

    alingment changes

--- src/com/streamApi/Employee.java
+++ src/com/streamApi/Employee.java
@@ -3,21 +3,24 @@ package com.streamApi;
 public class Employee {
 	String ename;
 	String desg;
+
 	public Employee(String ename, String desg) {
 		super();
 		this.ename = ename;
 		this.desg = desg;
 	}
+
 	public String getEname() {
 		return ename;
 	}
+
 	public String getDesg() {
 		return desg;
 	}
+
 	@Override
 	public String toString() {
 		return "Employee [ename=" + ename + ", desg=" + desg + "]";
 	}
-
 
 }
