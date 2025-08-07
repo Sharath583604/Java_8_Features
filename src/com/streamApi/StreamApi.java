7535834ee3bdf7f1acc7e0ca6e9020ef881a9352
Sharath583604 <sharathjogi04@gmail.com>
2025-08-07 12:19:47 +0530

    changes

--- src/com/streamApi/StreamApi.java
+++ src/com/streamApi/StreamApi.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 public class StreamApi {
 	public static void main(String[] args) {
 		
-		//STREAM 
+		//STREAM
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		Stream<Integer> stream = list.stream();
 		stream.filter(i -> (i % 2 == 0)).forEach(System.out::println);
